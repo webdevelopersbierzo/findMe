f781905a0a621f2434f7c811e5a5d57ce485e333
oscar <oscar@escritorio.local>
2021-12-07 09:48:20 +0100

    amodificado modelo candidate_schema.js

--- database/migrations/1638866006024_candidate_schema.js
+++ database/migrations/1638866006024_candidate_schema.js
@@ -6,7 +6,18 @@ const Schema = use('Schema')
 class CandidateSchema extends Schema {
   up () {
     this.create('candidates', (table) => {
-      table.increments()
+      table.increments('candidateId')
+      table.string('namefull',100),notNullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('telefono', 254).notNullable()
+      table.timestamps('nacimiento',{useTz:true})
+      table.string('salarioActual', 10).notNullable()
+      table.string('salarioDeseado', 10).notNullable()
+      table.string('localidad', 254).notNullable().unique()
+      table.string('pais', 254).notNullable().unique()
+      table.boolean('remoto').notNullable
+      table.boolena('movilidad'),notNullable()
+      table.integer('userId').unsigned().references('userId').inTable('user')
       table.timestamps()
     })
   }
