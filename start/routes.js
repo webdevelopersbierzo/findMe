3c9a4a7877f804e21bbd16989a3216dcd239fb90
oscar <oscar@escritorio.local>
2021-12-09 15:09:39 +0100

    Creadas rutas CRUD para user y candidate en routes.js

--- start/routes.js
+++ start/routes.js
@@ -15,7 +15,49 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-
+/* pagina principal*/
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+/* ----CRUD USER */
+//create
+Route.post('/user', ()=> {
+  return {greeting: "Hola creando un usuario"}
+})
+// Read
+Route.get('/user/:id', ()=> {
+  return {greeting: "Hola comprobando usuario"}
+})
+// update
+Route.put('/user/:id', ()=>{
+  return {greeting: 'Actualizando un usuario'}
+})
+// Delete
+Route.delete('/user/:id', ()=>{
+  return{greeting: 'Borrando un usuario'}
+})
+// ---CRUD CANDIDATOS
+// Create 
+Route.post('/candidate/:id',()=>{
+  return {greeting: "Modificando un candidato"}
+ })
+// Read 
+Route.get('/candidate/:id', ()=>{
+  return { greeting: 'Soy el candidato {id}'}
+})
+// Update 
+Route.put('/candidate/:id',()=>{
+  return {greeting: "Modificando un candidato"}
+ })
+// Delete 
+Route.delete('/candidate/:id',()=>{
+  return {greeting: "Borrando un candidato"}
+ })
+/*lista de candidatos*/
+Route.get('/candidates', ()=>{
+  return { greeting: 'Hola Listado de candidate'}
+})
+
+
+
