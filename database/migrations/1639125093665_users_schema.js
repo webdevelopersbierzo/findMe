82b8de056279c065bf70aa68cea62179ac59b5c6
oscar <oscar@escritorio.local>
2021-12-10 09:54:33 +0100

    corrigiendo modelos para actualizacion de la base de datos 2

--- database/migrations/1639125093665_users_schema.js
+++ database/migrations/1639125093665_users_schema.js
@@ -3,7 +3,7 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class UserSchema extends Schema {
+class UsersSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments('userId')
@@ -19,4 +19,4 @@ class UserSchema extends Schema {
   }
 }
 
-module.exports = UserSchema
+module.exports = UsersSchema
