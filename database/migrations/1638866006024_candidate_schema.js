61b4280497dbb6f83cf24ab01cdc82f5ca015f47
oscar <oscar@escritorio.local>
2021-12-10 09:10:32 +0100

    completada migracion de candidate

--- database/migrations/1638866006024_candidate_schema.js
+++ database/migrations/1638866006024_candidate_schema.js
@@ -8,16 +8,16 @@ class CandidateSchema extends Schema {
     this.create('candidates', (table) => {
       table.increments('candidateId')
       table.string('namefull',100).notNullable()
-      //table.string('email', 254).notNullable().unique()
-      //table.string('telefono', 10).notNullable()
-      //table.date('nacimiento')
-      //table.string('salarioActual', 10).notNullable()
-      //table.string('salarioDeseado', 10).notNullable()
-      //table.string('localidad', 254).notNullable().unique()
-      //table.string('pais', 254).notNullable().unique()
-      //table.boolean('remoto').notNullable
-      //table.boolean('movilidad').notNullable()
-      //table.integer('id').unsigned().references('userId').inTable('user')
+      table.string('email', 254).notNullable().unique()
+      table.string('telefono', 10).notNullable()
+      table.date('nacimiento')
+      table.string('salarioActual', 10).notNullable()
+      table.string('salarioDeseado', 10).notNullable()
+      table.string('localidad', 254).notNullable().unique()
+      table.string('pais', 254).notNullable().unique()
+      table.boolean('remoto').notNullable
+      table.boolean('movilidad').notNullable()
+      table.integer('id').unsigned().references('userId').inTable('user')
       table.timestamps()
     })
   }
